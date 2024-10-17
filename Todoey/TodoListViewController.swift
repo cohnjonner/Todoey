911442649b0777022828a7887f0649e48eca44d5
Mark Brumbaugh <brumbaugh.mark@gmail.com>
2024-10-16 20:17:22 -0700

    rewrote loadItems function

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -31,7 +31,7 @@ class TodoListViewController: UITableViewController {
         
         
         
-        //loadItems()
+        loadItems()
         
     }
     
@@ -100,21 +100,14 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    /*func loadItems(){
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            
-            
-            do{
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Some Error happened \(error)")
-            }
-            
-            
-            
+    func loadItems(){
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do{
+            itemArray = try context.fetch(request)
+        } catch {
+            print("error fetching data from context \(error)")
         }
-    }*/
+    }
     
     
 }
